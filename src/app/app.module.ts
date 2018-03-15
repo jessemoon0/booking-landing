680712b0dc53de0fda77160939e7b59c8a01aa67
Jessie J Valladares <shajessie@me.com>
2018-03-14 22:07:41 -0400

    Initializing flexbox project design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { ImagesComponent } from './main/images/images.component';
+import { TitleComponent } from './main/title/title.component';
+import { ContentComponent } from './main/content/content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainComponent,
+    ImagesComponent,
+    TitleComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule

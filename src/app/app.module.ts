6bfcce1ea824410ee3a8a3d79557fef70df41457
Jessie J Valladares <shajessie@me.com>
2018-03-24 16:02:21 -0400

    Styling header and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MainComponent } from './main/main.component';
-import { ImagesComponent } from './main/images/images.component';
-import { TitleComponent } from './main/title/title.component';
-import { ContentComponent } from './main/content/content.component';
+import { MainComponent } from './sidebar/main/main.component';
+import { ImagesComponent } from './sidebar/main/images/images.component';
+import { TitleComponent } from './sidebar/main/title/title.component';
+import { ContentComponent } from './sidebar/main/content/content.component';
 
 
 @NgModule({
